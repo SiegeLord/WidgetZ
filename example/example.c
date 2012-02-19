435b0afa3c7f4e575cb899d569893ec48e6cba96
Karthik Kumar Viswanathan <kviswanathan@zynga.com>
2012-02-20 00:52:11 +0530

    Get Example to Show on iOS

--- example/example.c
+++ example/example.c
@@ -36,6 +36,8 @@ int main(int argc, char *argv[])
 	ALLEGRO_FONT *font;
 	ALLEGRO_EVENT_QUEUE *queue;
 	int refresh_rate;
+    float size=1.0;
+    int font_size=18;
 	double fixed_dt;
 	double old_time;
 	double game_time;
@@ -48,10 +50,16 @@ int main(int argc, char *argv[])
 	al_init_font_addon();
 	al_init_primitives_addon();
 	al_init_ttf_addon();
+#ifdef IPHONE
+    size=0.5;
+    al_set_new_display_option(ALLEGRO_SUPPORTED_ORIENTATIONS, ALLEGRO_DISPLAY_ORIENTATION_LANDSCAPE, 1);
+    display = al_create_display(0, 0);
+#else //IPHONE
 	display = al_create_display(1000, 600);
+#endif //IPHONE
 	al_install_keyboard();
 	// Load a font
-	font = al_load_font("DejaVuSans.ttf", 18, 0);
+	font = al_load_font("DejaVuSans.ttf", font_size*size, 0);
 	
 	al_install_mouse();
 	
@@ -84,27 +92,27 @@ int main(int argc, char *argv[])
 	/*
 	Define all other gui elements, fill_layout is an automatic layout container
 	*/
-	wz_create_fill_layout(gui, 50, 50, 300, 450, 50, 20, WZ_ALIGN_CENTRE, WZ_ALIGN_TOP, -1);
+	wz_create_fill_layout(gui, 50*size, 50*size, 300*size, 450*size, 50*size, 20*size, WZ_ALIGN_CENTRE, WZ_ALIGN_TOP, -1);
 	
-	wz_create_textbox(gui, 0, 0, 200, 50, WZ_ALIGN_CENTRE, WZ_ALIGN_CENTRE, al_ustr_new("Welcome to WidgetZ!"), 1, -1);
-	wz_create_toggle_button(gui, 0, 0, 200, 50, al_ustr_new("Toggle 1"), 1, 1, 5);
-	wz_create_toggle_button(gui, 0, 0, 200, 50, al_ustr_new("Toggle 2"), 1, 1, 6);
-	wz_create_toggle_button(gui, 0, 0, 200, 50, al_ustr_new("Toggle 3"), 1, 1, 7);
+	wz_create_textbox(gui, 0*size, 0*size, 200*size, 50*size, WZ_ALIGN_CENTRE, WZ_ALIGN_CENTRE, al_ustr_new("Welcome to WidgetZ!"), 1, -1);
+	wz_create_toggle_button(gui, 0*size, 0*size, 200*size, 50*size, al_ustr_new("Toggle 1"), 1, 1, 5);
+	wz_create_toggle_button(gui, 0*size, 0*size, 200*size, 50*size, al_ustr_new("Toggle 2"), 1, 1, 6);
+	wz_create_toggle_button(gui, 0*size, 0*size, 200*size, 50*size, al_ustr_new("Toggle 3"), 1, 1, 7);
 	
-	wgt = (WZ_WIDGET*)wz_create_button(gui, 0, 0, 200, 50, al_ustr_new("Quit"), 1, 1);
+	wgt = (WZ_WIDGET*)wz_create_button(gui, 0*size, 0*size, 200*size, 50*size, al_ustr_new("Quit"), 1, 1);
 	wz_set_shortcut(wgt, ALLEGRO_KEY_Q, ALLEGRO_KEYMOD_CTRL);
 	
-	wz_create_fill_layout(gui, 350, 50, 300, 450, 50, 20, WZ_ALIGN_CENTRE, WZ_ALIGN_TOP, -1);
+	wz_create_fill_layout(gui, 350*size, 50*size, 300*size, 450*size, 50*size, 20*size, WZ_ALIGN_CENTRE, WZ_ALIGN_TOP, -1);
 	
-	wz_create_textbox(gui, 0, 0, 200, 50, WZ_ALIGN_CENTRE, WZ_ALIGN_CENTRE, al_ustr_new("Scroll Bars:"), 1, -1);
-	wz_create_scroll(gui, 0, 0, 200, 50, 20, 9);
-	wz_create_scroll(gui, 0, 0, 50, 200, 20, 9);
-	wz_create_scroll(gui, 0, 0, 50, 200, 20, 9);
+	wz_create_textbox(gui, 0*size, 0*size, 200*size, 50*size, WZ_ALIGN_CENTRE, WZ_ALIGN_CENTRE, al_ustr_new("Scroll Bars:"), 1, -1);
+	wz_create_scroll(gui, 0*size, 0*size, 200*size, 50*size, 20*size, 9);
+	wz_create_scroll(gui, 0*size, 0*size, 50*size, 200*size, 20*size, 9);
+	wz_create_scroll(gui, 0*size, 0*size, 50*size, 200*size, 20*size, 9);
 	
-	wz_create_fill_layout(gui, 650, 50, 300, 450, 20, 20, WZ_ALIGN_CENTRE, WZ_ALIGN_TOP, -1);
-	wz_create_textbox(gui, 0, 0, 200, 50, WZ_ALIGN_CENTRE, WZ_ALIGN_CENTRE, al_ustr_new("Edit Box:"), 1, -1);
-	wgt = (WZ_WIDGET*)wz_create_editbox(gui, 0, 0, 200, 50, al_ustr_new("Type here..."), 1, -1);
-	wz_create_textbox(gui, 0, 0, 200, 50, WZ_ALIGN_LEFT, WZ_ALIGN_TOP, al_ustr_new("A textbox with a lot of text"
+	wz_create_fill_layout(gui, 650*size, 50*size, 300*size, 450*size, 20*size, 20*size, WZ_ALIGN_CENTRE, WZ_ALIGN_TOP, -1);
+	wz_create_textbox(gui, 0*size, 0*size, 200*size, 50*size, WZ_ALIGN_CENTRE, WZ_ALIGN_CENTRE, al_ustr_new("Edit Box:"), 1, -1);
+	wgt = (WZ_WIDGET*)wz_create_editbox(gui, 0*size, 0*size, 200*size, 50*size, al_ustr_new("Type here..."), 1, -1);
+	wz_create_textbox(gui, 0*size, 0*size, 200*size, 50*size, WZ_ALIGN_LEFT, WZ_ALIGN_TOP, al_ustr_new("A textbox with a lot of text"
 					  " in it. Also supports new lines:\n\nNew paragraph.\n"
 					  "Also supports unicode:\n\n"
 					  "Привет"), 1, -1);
